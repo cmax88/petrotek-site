d322f08155972ee5fa4ecf49145b72a29a9d9f3d
cmax88 <cjmax88@gmail.com>
2026-02-11 15:44:05 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
-// src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+// 1. Import HashRouter instead of BrowserRouter
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* 2. Wrap App in HashRouter and set the basename */}
+    <HashRouter basename="/petrotek-site">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
